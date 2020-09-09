4bad907be6eb68be4d57bf8d78e0cc157dafc811
Nandish <nandish.trivedi0409@gmail.com>
2020-09-09 15:37:47 +0530

    commit4

--- lib/screens/pageviews/chat_list_screen.dart
+++ lib/screens/pageviews/chat_list_screen.dart
@@ -3,6 +3,7 @@ import 'package:video_app1/resources/firebase_repository.dart';
 import 'package:video_app1/utils/universal_variables.dart';
 import 'package:video_app1/utils/utilities.dart';
 import 'package:video_app1/widgets/appbar.dart';
+import 'package:video_app1/widgets/custom_tile.dart';
 
 class ChatListScreen extends StatefulWidget {
   @override
@@ -61,6 +62,73 @@ class _ChatListScreenState extends State<ChatListScreen> {
     return Scaffold(
       backgroundColor: UniversalVariables.blackColor,
       appBar: customAppBar(context),
+      floatingActionButton: NewChatButton(),
+      body: ChatListContainer(currentUserId),
+    );
+  }
+}
+
+class ChatListContainer extends StatefulWidget {
+  final String currentUserId;
+
+  ChatListContainer(this.currentUserId);
+  @override
+  _ChatListContainerState createState() => _ChatListContainerState();
+}
+
+class _ChatListContainerState extends State<ChatListContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return CustomTile(
+            mini: false,
+            onTap: () {},
+            title: Text(
+              "This is Nandish",
+              style: TextStyle(
+                  color: Colors.white, fontFamily: "Arial", fontSize: 19),
+            ),
+            subtitle: Text(
+              "Hello",
+              style: TextStyle(
+                color: UniversalVariables.greyColor,
+                fontSize: 14,
+              ),
+            ),
+            leading: Container(
+              constraints: BoxConstraints(maxHeight: 60, maxWidth: 60),
+              child: Stack(
+                children: <Widget>[
+                  CircleAvatar(
+                    maxRadius: 30,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: NetworkImage(
+                        "https://yt3.ggpht.com/a/AGF-l7_zT8BuWwHTymaQaBptCy7WrsOD72gYGp-puw=s900-c-k-c0xffffffff-no-rj-mo"),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: UniversalVariables.onlineDotColor,
+                          border: Border.all(
+                            color: UniversalVariables.blackColor,
+                            width: 2,
+                          )),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -106,6 +174,23 @@ class UserCircle extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class NewChatButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: UniversalVariables.fabGradient,
+          borderRadius: BorderRadius.circular(50)),
+      child: Icon(
+        Icons.edit,
+        color: Colors.white,
+        size: 25,
+      ),
+      padding: EdgeInsets.all(15),
     );
   }
 }
